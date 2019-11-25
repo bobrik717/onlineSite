ea6a3a8589b5fc1cf559310d78b0d2e1133e3989
KVasiliev717 <KVasiliev@servicefusion.com>
2019-11-25 12:37:31 +0200

    aws

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-
-echo "<pre>";
-print_r(11111111111);
-echo "</pre>";
-die;
-
 ini_set('error_reporting', E_ERROR, E_WARNING);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
