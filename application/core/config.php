0977231838a0a7341933357bebe5abecf613ecd3
KVasiliev717 <KVasiliev@servicefusion.com>
2019-11-25 14:38:32 +0200

    aws

--- application/core/config.php
+++ application/core/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DBHOST','localhost');
-define('DBLOGIN','fb3809c9nb_cms');
-define('DBPASS','8y!e6H6i');
-define('DBNAME','fb3809c9nb_cms');
+define('DBHOST','us-east-2.console.aws.amazon.com');
+define('DBLOGIN','root');
+define('DBPASS','123456789');
+define('DBNAME','ebdb');
 define('MAIN_ROOT','/');
 define('PUBKEY','6LePJvwSAAAAACPo1eUZ7m19_EkjNSSQCdqYa6wh');
 define('PRIVKEY','6LePJvwSAAAAALA1r2upbfw4S6S6Bwxvov_CgB9m');
